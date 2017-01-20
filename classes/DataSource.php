fd7c69949e46a133d02d093bd2e2781fccf244a4
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-01-20 11:59:43 +0200

    Added entries to datasource

--- classes/DataSource.php
+++ classes/DataSource.php
@@ -1,84 +1,105 @@
 <?php
 /**
- * Implements a data source. 
- */
+ * Implements a data source.
+*/
 class DataSource {
 	private $strDriverName;
 	private $tblDriverOptions;
-	private $strHostName;
+	private $strHost;
+	private $intPort;
 	private $strUserName;
 	private $strPassword;
-	
+	private $strSchema;
+
 	/**
 	 * Sets database server driver name.
-	 * 
+	 *
 	 * @param string $strDriverName
 	 * @return void
 	 */
 	public function setDriverName($strDriverName) {
 		$this->strDriverName = $strDriverName;
 	}
-	
+
 	/**
 	 * Gets database server vendor.
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getDriverName() {
 		return $this->strDriverName;
 	}
-	
+
 	/**
 	 * Sets database server vendor options
-	 * 
+	 *
 	 * @param array $tblDriverOptions
 	 * @return void
 	 */
 	public function setDriverOptions($tblDriverOptions) {
 		$this->tblDriverOptions = $tblDriverOptions;
 	}
-	
+
 	/**
 	 * Gets driver options
-	 * 
+	 *
 	 * @return array
 	 */
 	public function getDriverOptions() {
 		return $this->tblDriverOptions;
 	}
-	
+
 	/**
 	 * Sets database server host name
-	 * 
-	 * @param string $strHostName
+	 *
+	 * @param string $strHost
 	 * @return void
 	 */
-	public function setHostName($strHostName) {
-		$this->strHostName = $strHostName;
+	public function setHost($strHost) {
+		$this->strHost = $strHost;
 	}
-	
+
 	/**
 	 * Gets database server host name
-	 * 
+	 *
 	 * @return string
 	 */
-	public function getHostName() {
-		return $this->strHostName;
+	public function getHost() {
+		return $this->strHost;
 	}
-	
+
+	/**
+	 * Sets database server port
+	 *
+	 * @param integer $intPort
+	 * @return void
+	 */
+	public function setPort($intPort) {
+		$this->intPort = $intPort;
+	}
+
+	/**
+	 * Gets database server port
+	 *
+	 * @return integer
+	 */
+	public function getPort() {
+		return $this->intPort;
+	}
+
 	/**
 	 * Sets database server user name
-	 * 
+	 *
 	 * @param string $strUserName
 	 * @return void
 	 */
 	public function setUserName($strUserName){
 		$this->strUserName = $strUserName;
 	}
-	
+
 	/**
 	 * Gets database server user name
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getUserName() {
@@ -102,5 +123,24 @@ class DataSource {
 	 */
 	public function getPassword() {
 		return $this->strPassword;
+	}
+
+	/**
+	 * Sets database server default schema
+	 *
+	 * @param string $strSchema
+	 * @return void
+	 */
+	public function setSchema($strSchema) {
+		$this->strSchema = $strSchema;
+	}
+
+	/**
+	 * Gets database server default schema
+	 *
+	 * @return string
+	 */
+	public function getSchema() {
+		return $this->strSchema;
 	}
 }
